e2e7b5e295163fe7532d176d2b00690a44e6cd0f
sagardeshraj <saagardeshraj14@gmail.com>
2023-03-28 14:56:19 +0530

    adding port 3000

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -40,7 +40,7 @@ function GamePage() {
       host:'ill-rose-tick-tie.cyclic.app/peerjs',
       path:'/',
       secure:true,
-      port:9000
+      port:3000
     });
 
     peer.on("call", (call) => {
